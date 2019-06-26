47108c12ddb8efaa52107cf72e99dde31345407e
Almog <almoggit@gmail.com>
2019-06-26 14:27:57 +0300

    update

--- app/src/main/java/com/example/lostandfoundnew/MainActivity.java
+++ app/src/main/java/com/example/lostandfoundnew/MainActivity.java
@@ -11,9 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FlowingDrawer ff;
-
-
+        FlowingDrawer fff;
         int b;
+        int g;
     }
 }
