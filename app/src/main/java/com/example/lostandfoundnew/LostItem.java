3dfd1cb4a2f22677898042def49de83c45f0fb56
Almog <almoggit@gmail.com>
2019-06-26 16:38:01 +0300

    update

---
+++ app/src/main/java/com/example/lostandfoundnew/LostItem.java
@@ -0,0 +1,14 @@
+package com.example.lostandfoundnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LostItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lost_item);
+    }
+}
--- app/src/main/java/com/example/lostandfoundnew/MainActivity.java
+++ app/src/main/java/com/example/lostandfoundnew/MainActivity.java
@@ -3,13 +3,11 @@ package com.example.lostandfoundnew;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
 import android.provider.Telephony;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -37,8 +35,7 @@ import hotchemi.android.rate.OnClickButtonListener;
 public class MainActivity extends AppCompatActivity {
     public static final int ERROR_DIALOG_REQ = 9001;
 
-    Activity activity;
-    Button btnFound;
+    Button btnFound, btnLost, btnLoginMenu;
     Context context;
     FirebaseAuth firebaseAuth;
     DatabaseReference lastOnlineRef;
@@ -52,24 +49,26 @@ public class MainActivity extends AppCompatActivity {
     TextView txtaboutus;
     TextView txtpostfound;
     TextView txtpostlost;
+    TextView tVinboxMenu, tVprofileMenu;
 
     protected void onCreate(Bundle paramBundle) {
         super.onCreate(paramBundle);
         setContentView(R.layout.activity_main);
-        this.btnFound = findViewById(R.id.btnfound);
-        Button button1 = findViewById(R.id.btnlost);
-        Button button2 = findViewById(R.id.btnloginmenu);
-        TextView textView1 = findViewById(R.id.txtviewinboxmenu);
-        this.firebaseAuth = FirebaseAuth.getInstance();
-        TextView textView2 = findViewById(R.id.txtviewprofilemenu);
-        this.relativeLayout = findViewById(R.id.mainrelativelayout);
-        this.txtpostfound = findViewById(R.id.txtpostfound);
-        this.txtpostlost = findViewById(R.id.txtpostlost);
-        this.txtaboutus = findViewById(R.id.txtaboutus);
-        this.activity = this;
-        this.context = this;
-        this.stopnotifyref = FirebaseDatabase.getInstance().getReference("token");
-        textView1.setOnClickListener(new View.OnClickListener() {
+
+        btnFound = findViewById(R.id.btnfound);
+        btnLost = findViewById(R.id.btnlost);
+        btnLoginMenu = findViewById(R.id.btnloginmenu);
+        tVinboxMenu = findViewById(R.id.txtviewinboxmenu);
+        firebaseAuth = FirebaseAuth.getInstance();
+        tVprofileMenu = findViewById(R.id.txtviewprofilemenu);
+        relativeLayout = findViewById(R.id.mainrelativelayout);
+        txtpostfound = findViewById(R.id.txtpostfound);
+        txtpostlost = findViewById(R.id.txtpostlost);
+        txtaboutus = findViewById(R.id.txtaboutus);
+
+        stopnotifyref = FirebaseDatabase.getInstance().getReference("token");
+
+        tVinboxMenu.setOnClickListener(new View.OnClickListener() {
             public void onClick(View param1View) {
                 if (MainActivity.this.firebaseAuth.getCurrentUser() != null) {
                     Intent intent = new Intent(MainActivity.this.getBaseContext(), Telephony.Sms.Inbox.class);
@@ -93,14 +92,14 @@ public class MainActivity extends AppCompatActivity {
         AppRate.showRateDialogIfMeetsConditions(this);
         this.txtaboutus.setOnClickListener(new View.OnClickListener() {
             public void onClick(View param1View) {
-                Intent intent = new Intent(MainActivity.this.getBaseContext(), About_Us.class);
+                Intent intent = new Intent(MainActivity.this.getBaseContext(), AboutUs.class);
                 MainActivity.this.startActivity(intent);
             }
         });
         this.txtpostfound.setOnClickListener(new View.OnClickListener() {
             public void onClick(View param1View) {
                 if (MainActivity.this.firebaseAuth.getCurrentUser() != null) {
-                    Intent intent = new Intent(MainActivity.this.getBaseContext(), Item_Upload.class);
+                    Intent intent = new Intent(MainActivity.this.getBaseContext(), ItemUpload.class);
                     intent.putExtra("status", 0);
                     MainActivity.this.startActivity(intent);
                     return;
@@ -118,7 +117,7 @@ public class MainActivity extends AppCompatActivity {
         this.txtpostlost.setOnClickListener(new View.OnClickListener() {
             public void onClick(View param1View) {
                 if (MainActivity.this.firebaseAuth.getCurrentUser() != null) {
-                    Intent intent = new Intent(MainActivity.this.getBaseContext(), Item_Upload.class);
+                    Intent intent = new Intent(MainActivity.this.getBaseContext(), ItemUpload.class);
                     intent.putExtra("status", 1);
                     MainActivity.this.startActivity(intent);
                     return;
@@ -134,8 +133,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         if (this.firebaseAuth.getCurrentUser() != null) {
-            button2.setText("log out");
-            button2.setOnClickListener(new View.OnClickListener() {
+            btnLoginMenu.setText("log out");
+            btnLoginMenu.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View param1View) {
                     String str = FirebaseAuth.getInstance().getCurrentUser().getUid();
                     MainActivity.this.stopnotifyref.child(str).child("token_id").setValue("notlogin");
@@ -144,26 +143,26 @@ public class MainActivity extends AppCompatActivity {
                     MainActivity.this.finish();
                     Intent intent = new Intent(MainActivity.this.getBaseContext(), MainActivity.class);
                     MainActivity.this.startActivity(intent);
-                    Toast.makeText(MainActivity.this.getApplicationContext(), "logout", 0).show();
+                    Toast.makeText(MainActivity.this.getApplicationContext(), "logout", Toast.LENGTH_SHORT).show();
                 }
             });
         } else {
-            button2.setText("log in");
-            button2.setOnClickListener(new View.OnClickListener() {
+            btnLoginMenu.setText("log in");
+            btnLoginMenu.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View param1View) {
                     Intent intent = new Intent(MainActivity.this.getBaseContext(), Login.class);
                     MainActivity.this.startActivity(intent);
                 }
             });
         }
-        textView2.setOnClickListener(new View.OnClickListener() {
+        tVprofileMenu.setOnClickListener(new View.OnClickListener() {
             public void onClick(View param1View) {
                 if (MainActivity.this.firebaseAuth.getCurrentUser() != null) {
-                    Intent intent = new Intent(MainActivity.this.getBaseContext(), Item_show.class);
+                    Intent intent = new Intent(MainActivity.this.getBaseContext(), ItemShow.class);
                     MainActivity.this.startActivity(intent);
                     return;
                 }
-                Snackbar snackbar = Snackbar.make(MainActivity.this.relativeLayout, "You Are not Login", 0).setAction("Login", new View.OnClickListener() {
+                Snackbar snackbar = Snackbar.make(MainActivity.this.relativeLayout, "You Are not Login", Snackbar.LENGTH_SHORT).setAction("Login", new View.OnClickListener() {
                     public void onClick(View param2View) {
                         Intent intent = new Intent(MainActivity.this.getBaseContext(), Login.class);
                         MainActivity.this.startActivity(intent);
@@ -186,28 +185,28 @@ public class MainActivity extends AppCompatActivity {
             public void onDrawerStateChange(int param1Int1, int param1Int2) {
                 if (param1Int2 == 0) {
                     Log.i("MainActivity", "Drawer STATE_CLOSED");
-                    dawerButton.setBackground(MainActivity.this.getResources().getDrawable(R.drawable.ic_menubar));
+                    dawerButton.setBackground(MainActivity.this.getResources().getDrawable(R.drawable.ic_menu_black_24dp));
                     return;
                 }
                 if (param1Int2 == 8) {
-                    dawerButton.setBackground(MainActivity.this.getResources().getDrawable(R.drawable.ic_back));
+                    dawerButton.setBackground(MainActivity.this.getResources().getDrawable(R.drawable.ic_arrow_back_black_24dp));
                     return;
                 }
             }
         });
-        this.btnFound.setOnClickListener(new View.OnClickListener() {
+        btnFound.setOnClickListener(new View.OnClickListener() {
             public void onClick(View param1View) {
-                Intent intent = new Intent(MainActivity.this.getApplicationContext(), Found_item.class);
+                Intent intent = new Intent(MainActivity.this.getApplicationContext(), FoundItem.class);
                 MainActivity.this.startActivity(intent);
             }
         });
-        button1.setOnClickListener(new View.OnClickListener() {
+        btnLost.setOnClickListener(new View.OnClickListener() {
             public void onClick(View param1View) {
-                Intent intent = new Intent(MainActivity.this.getApplicationContext(), Lost_item.class);
+                Intent intent = new Intent(MainActivity.this.getApplicationContext(), LostItem.class);
                 MainActivity.this.startActivity(intent);
             }
         });
-        imageButton.setOnClickListener(new View.OnClickListener() {
+        dawerButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View param1View) {
                 if (MainActivity.this.mDrawer.isMenuVisible()) {
                     MainActivity.this.mDrawer.closeMenu();
